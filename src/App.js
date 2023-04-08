392620263dc8823a3aab154710558ea6576532e2
Md. Khalid Hasan <shohag9438@gmail.com>
2023-04-08 22:39:44 +0600

    add pages to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./index.css"
 
+//calling pages to main app
+import Home from "./routes/Home"
+import Project from "./routes/Project";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import { Route, Routes } from "react-router-dom";
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+    {/* <h1>
+      This is a portfolio site.
+    </h1>   */}
+
+    <Routes>
+      <Route path="/" element = {<Home />} />
+      <Route path="/project" element = {<Project />} />
+      <Route path="/about" element = {<About />} />
+      <Route path="/contact" element = {<Contact />} />
+    </Routes>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom"
+
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
+
 
