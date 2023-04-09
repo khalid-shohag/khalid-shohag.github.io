0ba9e71b679385fd71b10181e0a9a3a3677b31a8
Md. Khalid Hasan <shohag9438@gmail.com>
2023-04-09 14:46:07 +0600

    Contact page is completed

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import HeroImgTwo from '../components/HeroImgTwo'
+import Form from '../components/Form'
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <HeroImgTwo heading="CONTACT." text="Let's have a chat" />
+      <Form />
       <Footer />
     </div>
   )
