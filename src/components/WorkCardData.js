7b5d03f85950401e564aa566f1486d358002382a
Md. Khalid Hasan <shohag9438@gmail.com>
2023-04-09 13:49:08 +0600

    Project page is completed

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,19 @@
+import one from '../assets/ponjika.jpg'
+import two from '../assets/abohaoya.jpg'
+
+const ProjectCardData = [
+    {
+        imgsrc: one,
+        title: "Calendar app.",
+        text: "This a basic calendar android app that can show data, time and if an user wants to schedule their tasks they can easily add those and will get reminder.",
+        link: "https://github.com/Khalid511/ponjika"
+    },
+    {
+        imgsrc: two,
+        title: "Basic weather app.",
+        text: "This a basic weather app makes with flutter that can show data, time and if an user wants to schedule their tasks they can easily add those and will get reminder.",
+        link: "https://github.com/Khalid511/abohaoya"
+    }
+]
+
+export default ProjectCardData;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import HeroImgTwo from '../components/HeroImgTwo'
+import Work from '../components/Work'
 
 const Project = () => {
   return (
     <div>
+       
       <Navbar />
       <HeroImgTwo heading="PROJECTS." text="some of my recent works" />
+      <Work/>
       <Footer />
+     
     </div>
   )
 }
